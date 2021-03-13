6a5be2ec261a8370cb5e6a757be89abfcd7c10fc
Michael <michael.c.hadley@gmail.com>
2021-03-13 17:32:15 -0500

    Added new user creation

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, Response
 from common.subreddits import Subreddit
+from common.user import User
 import jsonify
+import uuid
 import datetime
 import boto3
 
@@ -18,3 +20,11 @@ def new_subr():
   return {'name': str(new_sub.name),
   'date_created': str(new_sub.date_created)
   }, 200
+
+@app.route('/users/new', methods=['POST'])
+def new_user():
+   body = request.get_json()
+   current_time = datetime.datetime.now()
+   userid = uuid.uuid1()
+   new_user = User(**body, uid=userid, date_created=current_time)
+   return {'name': str(new_user.username), 'uuid': str(new_user.uid)}, 200
---
+++ api/common/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, uid, username, password, date_created):
+        self.uid = uid
+        self.username = username
+        self.password = password
+        self.date_created = date_created
